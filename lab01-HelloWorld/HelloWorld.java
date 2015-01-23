2c085fcfaf4172dc7eaab4553498bdc581c8c929
seh213 <seh213@lehigh.edu>
2015-01-23 19:53:45 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+//
+//Sewook Hur
+//print out Hello World
+
+//define a class
+
+public class HelloWorld{
+    
+    //add main method
+    
+    public static void main(String[] args){
+        
+        //print the statment
+        
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("testing");
+        
+        
+    }//end of method
+}//end of class
