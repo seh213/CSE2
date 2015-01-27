04f196a86c5eb59cac20c432dcb23eeaf4316e58
seh213 <seh213@lehigh.edu>
2015-01-27 21:16:19 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+//CSE002 HW1: Welcome Message
+//Spring 2015 Homework Assignment 1
+//2015 Jan 27
+//Sewook Hur
+
+//print out displays to the screen a welcome from you, 
+//using your Lehigh network ID as a signature.
+
+//define a class
+
+public class WelcomeClass{
+    
+    //add main method
+    
+    public static void main(String[] args){
+        
+        //print the statment
+
+        System.out.println("  -------------     ");
+        System.out.println("  |  WELCOME  |     ");
+        System.out.println("  -------------     ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-S--E--H--2--1--3->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /" );
+        System.out.println("  v  v  v  v  v  v"  );
+        System.out.println("This winter break I went to South Korea and met my family.");
+        System.out.println("It was best winter break in my life");
+
+        
+    }//end of method
+}//end of class
